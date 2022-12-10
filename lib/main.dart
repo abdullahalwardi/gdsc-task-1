740a1b7bc70930b129e7e5c214261fc9216e48d9
montadhar99 <mntther307@gmail.com>
2022-12-10 15:29:08 +0300

    final edit of task

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,
     home: homepage(),
   ));
 }
 
 class homepage extends StatelessWidget {
   const homepage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("GDSC AL-iraqia"),
         centerTitle: true,
-        backgroundColor: Colors.indigo,
+        backgroundColor: Color(0xFF2d3447),
       ),
       drawer: Drawer(),
       body: Container(
