3151ef0561efe35e987960f98fb0e7a0eaae9728
Churro30 <baneen30churro@gmail.com>
2023-01-13 23:52:11 +0300

    Baneen-Ahmed-Task1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,64 +5,90 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("GDSC Al-iraqia"),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(252, 49, 55, 80),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Text(
-                  "Let's dart together!",
-                  style: TextStyle(
-                      fontSize: 28,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Flutter Team",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.blue.shade900,
-                  ),
-                ),
-              ],
-            ),
-            CircleAvatar(
-                radius: 130,
-                backgroundColor: Color.fromARGB(255, 169, 219, 255),
-                child: Image(image: AssetImage("assets/images/gdsc.png"))),
-            Text(
-              "Noor Basim Mohammed",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.blue.shade900,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("GDSC Al-iraqia"),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 43, 53, 68),
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  const Task1Text(
+                      text: "Let's dart together!",
+                      size: 35,
+                      color: Colors.black),
+                  Task1Text(
+                      text: "Flutter team",
+                      size: 20,
+                      color: Colors.blue.shade200),
+                ],
+              ),
+              CircleAvatar(
+                backgroundImage:
+                    const AssetImage("assets/images/googleclub.png"),
+                backgroundColor: Colors.pink.shade300,
+                radius: 150,
+              ),
+              const Task1Text(
+                text: "Baneen Ahmed",
+                size: 20,
+                color: Colors.black,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Task1Text extends StatelessWidget {
+  const Task1Text({
+    Key? key,
+    required this.color,
+    required this.size,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+  final double size;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.bold,
+        color: color,
       ),
     );
   }
